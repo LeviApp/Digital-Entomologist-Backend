f297bef045536f11d7c0de8ee23e9e7a20191e25
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-08-08 20:55:59 -0600

    C# project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace The_Digital_Entomologist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
