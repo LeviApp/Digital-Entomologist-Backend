43c58f681cc733443a2b7d34b93c17ddfcb7e1e6
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-04-11 17:21:31 -0600

    changed to a nodejs project

---
+++ index.js
@@ -0,0 +1,18 @@
+// db.solutions.insert({
+//     id: 1,
+//     user: {
+//     author: "LeviApp"
+//     },
+//     tech: "Git",
+//     title: "No Such File or Directory",
+//     description: "Discover how to enter into a new directory or file.",
+//     thumbnail: "TerminalPic",
+//     headings: ["Step 1: View Contents of Current Directory", "Step 2: Write the Correct Name", "SUCCESS!"],
+//     body: "Step 1: View Contents of Current Directory. Type 'ls' in the Terminal. View the contents of the folder you are currently in. Step 2: Write the Correct Name. Find the folder or file you are trying to enter. Pay attention to the spelling. Type cd (NAME OF FOLDER). SUCCESS!",
+//     imgs: ["lsImg", "cdImg"],
+//     vids: ["lsVid", "cdVid"],
+//     likes: 4444,
+//     views: 4444,
+//     tags: ["Git", "Folder", "Directory", "List", "cd", "ls"],
+//     date: Date()
+//     })
